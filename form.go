7b441873f9960b994c43b356b1b96843ce7648d9
Carlos Eduardo Lenz <carlos.lenz@gmail.com>
2017-07-04 22:46:11 -0300

    Extract from query strings and forms
    Fixes #3

---
+++ form.go
@@ -0,0 +1,37 @@
+package lookup
+
+import (
+	"net/http"
+
+	"github.com/carloslenz/epp"
+)
+
+type formLooker struct {
+	*http.Request
+}
+
+// NewForm returns a Looker to access r.Form.
+func NewForm(req *http.Request) Looker {
+	return &formLooker{
+		Request: req,
+	}
+}
+
+func (l *formLooker) LookupKey(k string) (string, bool, error) {
+	if err := l.ParseForm(); err != nil {
+		return "", false, epp.Wrap(err, "ParseForm failed")
+	}
+
+	v, ok := l.Form[k]
+	if !ok {
+		return "", false, nil
+	}
+	s := "1"
+	for _, val := range v {
+		if val != "" {
+			s = val
+			break
+		}
+	}
+	return s, true, nil
+}
